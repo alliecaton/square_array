ec1207577670513314915b1c1eeb046774b06c8c
Allie <alliecaton3@gmail.com>
2020-12-08 16:48:48 -0500

    commit tree e5656148b0fd7bfc998e90f678bb40d432abbcf5

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+    new_array = []
+    array.each do |number|
+    new_array << number**2
+    end
+    new_array
 end
+
+
+
+
+def square_array_collect(array)
+  array.collect {|number| number **2 }
+end
+
+
